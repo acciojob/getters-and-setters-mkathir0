0cf8b0d23e5360f3350ad9b54719e62d110593f0
Mithrajit Kathir <mithrajit07@gmail.com>
2026-05-15 10:34:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,44 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name:string,age:number){
+		this.name=name;
+		this.age=age;
+	}
 
-class Student extends Person {}
+	get()
+	{
+		return name;
+	}
 
-class Teacher extends Person {}
+	set()
+	{
+		return age;
+	}
+}
+
+class Student extends Person {
+	constructor(name,age){
+		this.name=name;
+		this.age=age;
+	}
+
+	void study()
+	{
+		return name + " is studying";
+	}
+}
+
+class Teacher extends Person {
+	constructor(name,age){
+		this.name=name;
+		this.age=age;
+	}
+
+	void teach()
+	{
+		return name+" is teaching"
+	}
+}
 
 // Do not change the code below this line
 window.Person = Person;
